ee9760756b9a3a18b2bcf239b142ffda5b3976b7
Cyril John Magayaga <93117076+Magayaga@users.noreply.github.com>
2024-03-17 11:13:11 +0800

    Update `shell.py` (v0.1.6-beta4)

--- src/shell.py
+++ src/shell.py
@@ -3,6 +3,7 @@
 # Copyright 2001-2024 Python Software Foundation (https://www.python.org/psf)
 
 import hyzero
+import sys
 
 version = "v0.1.6-beta4"
 date = "September 28, 2024"
@@ -10,14 +11,18 @@ date = "September 28, 2024"
 print(f"Hyzero {version} {date} - created & developed by Cyril John Magayaga")
 
 while True:
-	text = input('Hyzero > ')
-	if text.strip() == "": continue
-	result, error = hyzero.run('<stdin>', text)
+    text = input('Hyzero > ')
+    if text.strip() == "": 
+        continue
+    if text.strip() == "exit()" or (text.strip() == "" and "\x11" in text):
+        print("Exiting Hyzero programming language")
+        break
+    result, error = hyzero.run('<stdin>', text)
 
-	if error:
-		print(error.as_string())
-	elif result:
-		if len(result.elements) == 1:
-			print(repr(result.elements[0]))
-		else:
-			print(repr(result))
+    if error:
+        print(error.as_string())
+    elif result:
+        if len(result.elements) == 1:
+            print(repr(result.elements[0]))
+        else:
+            print(repr(result))
